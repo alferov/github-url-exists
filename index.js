6b46095919817d36efd8082014e63c804b33c71b
Philipp Alferov <philipp.alferov@gmail.com>
2015-10-12 00:28:11 -0400

    Implement input validation

--- index.js
+++ index.js
@@ -8,6 +8,17 @@
  * @return
  */
 
-module.exports = function isValidGithubUrl(url, opts, cb) {
+var isFunction = function(fn) {
+  return fn && {}.toString.call(fn) === '[object Function]';
+}
+
+module.exports = function (url, cb) {
+  if (!(typeof url === 'string' && url.length !== 0)) {
+    throw new TypeError('URL should be a string');
+  }
+
+  if (!(isFunction(cb))) {
+    throw new TypeError('Callback should be a function');
+  }
 
 };
--- test/test.js
+++ test/test.js
@@ -1,8 +1,15 @@
 'use strict';
 var chai = require('chai');
+var isValidURL = require('../index.js');
 var expect = chai.expect;
-var current, expected, source;
 
 describe('is-valid-github-url', function() {
+  it('should throw an error if passed arguments are not correct', function() {
+    expect(isValidURL.bind(null))
+      .to.throw(/should be a string/);
+
+    expect(isValidURL.bind(null, 'hello', null))
+      .to.throw(/should be a function/);
+  });
 
 });
